402d9ba5510ed6673b2a3dd549c8514be6456840
AbdurRahim999 <141570882+AbdurRahim999@users.noreply.github.com>
2025-09-08 20:00:23 +0530

    updating logIn-LogOut button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
 
 
 const Header = () => {
+
+  const [logInbtn, setLogInbtn]= useState("Log In");
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -15,6 +19,13 @@ const Header = () => {
           <li>About</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+          className="login-btn"
+          onClick={()=>{
+           logInbtn=== "Log In" ? setLogInbtn("Log Out") : setLogInbtn("Log In")  ;
+          }}>
+          {logInbtn}
+          </button>
         </ul>
       </div>
     </div>
